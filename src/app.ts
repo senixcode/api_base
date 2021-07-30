af3530ab82709884a3b84db050f1511a758c2d53
senixcode <codesenix@gmail.com>
2021-07-29 23:27:15 -0500

    feat: :sparkles: add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application } from 'express'
+import morgan from 'morgan'
 const app: Application = express()
 
 import authRoutes from './routes/auth'
 
 // setting
 app.set('port', 4000)
+//middlewares
+app.use(morgan('dev'))
 //routes
 app.use(authRoutes)
 

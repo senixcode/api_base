4ef3191755b525a200b1be8e13dcd5c3d7ff4d85
senixcode <codesenix@gmail.com>
2021-07-31 13:21:30 -0500

    feat: :sparkles: add cors initialized

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
+//initialization
 const app: Application = express()
 
 import authRoutes from './routes/auth'
@@ -8,6 +10,7 @@ import authRoutes from './routes/auth'
 app.set('port', 4000)
 //middlewares
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 //routes
 app.use('/api/auth', authRoutes)

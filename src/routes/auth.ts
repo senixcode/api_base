37e7f9398944173cada7ea1055019953538104ee
senixcode <codesenix@gmail.com>
2021-07-31 13:14:28 -0500

    feat: :sparkles: add TokenValidation to profile

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 const router: Router = Router()
 import * as AuthController from '../controllers/auth.controller'
+import TokenValidation from '../libs/verifyToken'
 router.post('/signup', AuthController.signup)
 router.post('/signin', AuthController.signin)
-router.get('/profile', AuthController.profile)
+router.get('/profile', TokenValidation, AuthController.profile)
 export default router

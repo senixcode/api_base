077efb53e72987b3a6e59f6d505ee8737e83b159
senixcode <codesenix@gmail.com>
2021-08-01 17:55:56 -0500

    feat: :sparkles: add try catch

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,30 +3,45 @@ import { day, getToken } from '../help/jwt'
 import User, { UserSchema } from '../models/User'
 
 export const signup = async (req: Req, res: Res) => {
-    const { username, email, password } = req.body
-    const findUser = await User.findOne({ email })
-    if (findUser) return res.status(401).json('this email is already registered')
-    const userNew: UserSchema = new User({
-        username,
-        email,
-        password,
-    })
-    const user = await userNew.save()
-    res.header('auth-token', getToken(user._id)).json(user)
+    try {
+        const { username, email, password } = req.body
+        const findUser = await User.findOne({ email })
+        if (findUser)
+            return res.status(401).json('this email is already registered')
+        const userNew: UserSchema = new User({
+            username,
+            email,
+            password,
+        })
+        const user = await userNew.save()
+        res.header('auth-token', getToken(user._id)).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
 export const signin = async (req: Req, res: Res) => {
-    const { email, password } = req.body
-    const findUser = await User.findOne({ email })
-    if (!findUser) return res.status(400).json("email or password is wrong")
-    const correctPassword: boolean = await findUser.comparePassword(password)
-    if (!correctPassword) return res.status(400).json('invalid password')
-    res.header('auth-token', getToken(findUser._id, { expiresIn: day })).json(findUser)
+    try {
+        const { email, password } = req.body
+        const findUser = await User.findOne({ email })
+        if (!findUser) return res.status(400).json('email or password is wrong')
+        const correctPassword: boolean = await findUser.comparePassword(password)
+        if (!correctPassword) return res.status(400).json('invalid password')
+        res
+            .header('auth-token', getToken(findUser._id, { expiresIn: day }))
+            .json(findUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
 export const profile = async (req: Req, res: Res) => {
-    const { userId } = req
-    const user = await User.findById(userId)
-    if (!user) return res.status(404).json('No User found')
-    res.json(user)
+    try {
+        const { userId } = req
+        const user = await User.findById(userId)
+        if (!user) return res.status(404).json('No User found')
+        res.json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }

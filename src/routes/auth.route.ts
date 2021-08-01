fdadc7eb13c88c2e2552b9c8b1fd794468ea68c0
senixcode <codesenix@gmail.com>
2021-08-01 17:56:35 -0500

    feat: :sparkles: add docs auth signup and signin

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,8 +6,94 @@ import TokenValidation from '../middleware/verifyToken'
 import SignInValidator from '../middleware/signInValidator'
 import SignUpValidator from '../middleware/signupValidator'
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *        type: object
+ *        properties:
+ *           _id: 
+ *             type: string
+ *             description: the user ID
+ *           username:
+ *              type: string
+ *           email: 
+ *              type: string
+ *           password:
+ *              type: string
+ *        required:
+ *          - username
+ *          - email
+ *          - password
+ *        example :
+ *          username: "only register"
+ *          email: "senix@gmail.com"
+ *          password: "password"                               
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Auths
+ *  description: Auth endpoint
+ */
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *  post:
+ *    summary: Register User, Returns the User and Token.
+ *    description: The Token in the headers as 'auth-token'.
+ *    tags: [Auths]
+ *    requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *              schema:
+ *                 $ref: '#/components/schemas/User'
+ *    responses:
+ *       200:
+ *         description: the registered user and token as header
+ *         content:
+ *           application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/User'
+ *       401:
+ *        description: this email is already registered
+ *       500:
+ *        description: Some server error                         
+ */
 router.post(RouteAuth.signup, SignUpValidator, AuthController.signup)
+
+
+/**
+ * @swagger
+ * /api/auth/signin:
+ *  post:
+ *    summary: Authenticate User, Returns the User and Token.
+ *    description: The Token in the headers as 'auth-token'.
+ *    tags: [Auths]
+ *    requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *              schema:
+ *                 $ref: '#/components/schemas/User'
+ *    responses:
+ *       200:
+ *         description: the registered user and token as header
+ *         content:
+ *           application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/User'
+ *       400:
+ *        description: email or password is wrong or invalid password'
+ *       500:
+ *        description: Some server error                         
+ */
 router.post(RouteAuth.signin, SignInValidator, AuthController.signin)
+
 router.get(RouteAuth.profile, TokenValidation, AuthController.profile)
 
 export default router

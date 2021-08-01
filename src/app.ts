2a921f916b3ee4f6a1aec2bec41f159dcf6769db
senixcode <codesenix@gmail.com>
2021-08-01 13:30:08 -0500

    feat: :sparkles: initial setup swagger

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+
+//swagger
+import swaggerUI from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
+import { options } from './swaggerOptions'
+
+// import routes
+import authRoutes from './routes/auth.route'
+
 //initialization
 const app: Application = express()
 
-import authRoutes from './routes/auth.route'
 
 // setting
 app.set('port', 4000)
+
 //middlewares
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 //routes
+const specs = swaggerJSDoc(options)
+
 app.use('/api/auth', authRoutes)
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs))
 
 export default app
---
+++ src/swaggerOptions.ts
@@ -0,0 +1,16 @@
+export const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: 'Api Base',
+            version: '1.0.0',
+            description: 'api base created by @senixcode',
+        },
+        servers: [
+            {
+                uri: "http://localhost:4000"
+            }
+        ]
+    },
+    apis: ["./src/routes/*ts"]
+}

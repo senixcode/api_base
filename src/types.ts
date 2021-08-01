0298b089ce06a53a0951848b34a55a00415d5cc6
senixcode <codesenix@gmail.com>
2021-08-01 10:33:06 -0500

    feat: :sparkles: add type UserOmitUsername

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,8 @@ export interface User {
     email: string,
     password: string
 }
-
+export type UserOmitUsername = Omit<User, 'username'>
+//export type Test = Pick<User, 'email' | 'password'>
 export interface Payload {
     _id: string,
     iat: number,

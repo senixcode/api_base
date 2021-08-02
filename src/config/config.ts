360a56c918471b1ddfb4129d6b4a9aaa7a601f6b
senixcode <codesenix@gmail.com>
2021-08-01 20:47:28 -0500

    feat: :sparkles: connectionUriMongoDb

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,10 @@
+import { NodeEnv } from "../emuns"
+const { MONGODB_URI, MONGODB_URI_TEST, NODE_ENV } = process.env
+const connectionUriMongoDb = NODE_ENV === NodeEnv.test ? MONGODB_URI_TEST : MONGODB_URI
 export default {
     JWT_SECRET: process.env.JWT_SECRET || 'someSecretToken',
     DB: {
-        URI: process.env.MONGODB_URI || 'mongodb://localhost/api_base',
+        URI: connectionUriMongoDb || 'mongodb://localhost/api_base',
 
     }
 }
